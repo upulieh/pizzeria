2db6d17408a43a79028f8c564ca7e03f695c2c86
Upulie Han <ireshaupulie@gmail.com>
2022-01-14 00:01:29 +0530

    Display sample data from db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
+import { useState, useEffect } from "react"; //useState hook
+import Axios from "axios";
+
+const baseURL = "http://localhost:3001"; //move to .env
 
 function App() {
-  return <div className="App"></div>;
+  const [listOfPizzas, setListOfPizzas] = useState([]);
+
+  //useEffect hooks run immediately when a website is loaded
+  // this is where backend api call is made
+  // where axios is used
+  useEffect(() => {
+    Axios.get(`${baseURL}/getPizzas`).then((res) => {
+      setListOfPizzas(res.data);
+    }); // get req, then use the promise to retrieve the data
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="pizzasDisplay">
+        {listOfPizzas.map((pizza) => {
+          return (
+            <div>
+              <h1>Pizza type: {pizza.pizzatype}</h1>
+              <h1>Small: {pizza.sprice}</h1>
+              <h1>Medium: {pizza.mprice}</h1>
+              <h1>Large: {pizza.lprice}</h1>
+            </div>
+          );
+        })}
+      </div>
+      <div>{/* <input type="text" placeholder="Name..."/> */}</div>
+    </div>
+  );
 }
 
 export default App;

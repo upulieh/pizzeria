af82ebb2a704dcf1dcdab8c9f329fc5dfd3e2750
Upulie Han <ireshaupulie@gmail.com>
2022-01-14 18:50:37 +0530

    Added a loading spinner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
-
 import { useState, useEffect } from "react"; //useState hook
 import Axios from "axios";
+// import { SpinnerCircular } from "spinners-react";
 
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Sidebar from "./components/Sidebar";
@@ -10,6 +10,7 @@ import Sidebar from "./components/Sidebar";
 const baseURL = "http://localhost:3001"; //move to .env
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [listOfPizzas, setListOfPizzas] = useState([]);
 
   //useEffect hooks run immediately when a website is loaded
@@ -19,14 +20,28 @@ function App() {
     Axios.get(`${baseURL}/getPizzas`).then((res) => {
       setListOfPizzas(res.data);
     }); // get req, then use the promise to retrieve the data
+
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
   }, []);
 
   return (
     <div className="App">
-      <Navbar />
-      <Sidebar />
-      <Home listOfPizzas={listOfPizzas} />
-      <div>{/* <input type="text" placeholder="Name..."/> */}</div>
+      {loading ? (
+        <div className="text-center">
+          {/* <SpinnerCircular сolor={"#3D2514"} enabled={loading} size={60} /> */}
+          <h2>Loading</h2>
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Sidebar />
+          <Home listOfPizzas={listOfPizzas} />
+          <div>{/* <input type="text" placeholder="Name..."/> */}</div>
+        </>
+      )}
     </div>
   );
 }
